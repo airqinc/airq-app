171be2c834c9934fbf51fa8ee1750396700d2c90
santy99ab <santy99ab@gmail.com>
2017-05-18 02:43:35 +0200

    .

--- web-server/controllers/dashboard.js
+++ web-server/controllers/dashboard.js
@@ -7,7 +7,7 @@ var express = require('express'),
 //
 //
 router.get('/', function(req, res) {
-  var measures_path = 'http://localhost:3000/diagnostics/Madrid/lastest'
+  var measures_path = 'http://localhost:3000/diagnostics/Madrid/latest'
   var request = require('request');
 
   function sortByKey(array, key) {
@@ -20,37 +20,34 @@ router.get('/', function(req, res) {
 
 
   if (req.session.user) {
-    request.post({
-         url: measures_path,
-         body:   {"isForecast": false,"number": 24}
-         }, function(error, response, body){
-            console.log(body);
-    },), function(error, response, body) {
-      console.log('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      diagnostics = sortByKey(JSON.parse(body), 'datetime').reverse() //sort by date, most recent first
-      chart_data = []
-      last_diagnostics.forEach(function(item, index) {
-        dominentpol = item['dominentpol']
-        temp = {
-          date: item['datetime'],
-          aqi: item['iaqi'][dominentpol],
-        }
-        chart_data[index] = temp
-      });
-      console.log(chart_data)
 
-      request.post({
-           url: measures_path,
-           body:   {"isForecast": true,"number": 24}
-           }, function(error, response, body){
-              console.log(body);
-      },), function(error, response, body) {
+
+      request.post({ url: measures_path , form: {"isForecast": false,"number": 24}},function(error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        diagnostics = sortByKey(JSON.parse(body), 'datetime').reverse() //sort by date, most recent first
+        chart_data = []
+        diagnostics.forEach(function(item, index) {
+          dominentpol = item['dominentpol']
+          temp = {
+            date: item['datetime'],
+            aqi: item['iaqi'][dominentpol],
+          }
+          chart_data[index] = temp
+        });
+        console.log(chart_data)
+        res.render('dashboard', {
+          user: req.session.user,
+          measures: JSON.stringify(chart_data)
+          })
+        });
+
+      /*request.post({ url: measures_path  , form: {"isForecast": true,"number": 24}},function(error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         forecasts = sortByKey(JSON.parse(body), 'datetime').reverse() //sort by date, most recent first
         chart_data_forecasts = []
-        last_diagnostics.forEach(function(item, index) {
+        forecasts.forEach(function(item, index) {
           dominentpol = item['dominentpol']
           temp = {
             date: item['datetime'],
@@ -58,14 +55,12 @@ router.get('/', function(req, res) {
           }
           chart_data_forecasts[index] = temp
         });
-        console.log(chart_data)
-
-      res.render('dashboard', {
-        user: req.session.user,
-        measures: JSON.stringify(chart_data),
-        measuresForecast:  JSON.stringify(chart_data_forecasts)
+        console.log(chart_data_forecasts)
+        res.render('dashboard', {
+          user: req.session.user,
+          measuresForecast:  JSON.stringify(chart_data_forecasts)
       })
-    });
+    });*/
   } else {
     res.redirect('/user/login');
   }
