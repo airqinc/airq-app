3eadbc4ddbbb4358b5971312b0c7518d02200ae4
santy99ab <santy99ab@gmail.com>
2017-05-18 23:20:03 +0200

    changes in web server

--- web-server/controllers/index.js
+++ web-server/controllers/index.js
@@ -3,11 +3,42 @@ var express = require('express'),
 
 router.use('/dashboard', require('./dashboard'))
 router.use('/user', require('./user'))
+router.use('/myZones',require('./myZones'))
 
 
 router.get('/', function(req, res) {
   if (req.session.user) {
     res.render('dashboard', {
+      user: req.session.user
+    })
+  } else {
+    res.redirect('/user/login');
+  }
+});
+
+router.get('/myZones', function(req, res) {
+  if (req.session.user) {
+    res.render('myZones', {
+      user: req.session.user
+    })
+  } else {
+    res.redirect('/user/login');
+  }
+});
+
+router.get('/otherZones', function(req, res) {
+  if (req.session.user) {
+    res.render('otherZones', {
+      user: req.session.user
+    })
+  } else {
+    res.redirect('/user/login');
+  }
+});
+
+router.get('/contact', function(req, res) {
+  if (req.session.user) {
+    res.render('contact', {
       user: req.session.user
     })
   } else {
